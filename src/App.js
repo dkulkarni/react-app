0f8f59757fbc1a81af164b1719c5ed143c2499e0
Deepthi Kulkarni <deepthi.kulkarni@redbubble.com>
2017-02-08 10:57:02 +1100

    Chapter 11: componentWillUnmount

--- src/App.js
+++ src/App.js
@@ -14,15 +14,22 @@ class App extends Component {
 
   componentWillMount() {
     console.log('componentWillMount');
+    this.setState({m: 2});
   }
 
   componentDidMount() {
     console.log('componentDidMount');
+    this.inc = setInterval(this.update, 500);
+  }
+
+  componentWillUnmount() {
+    console.log('componentWillUnmount');
+    clearInterval(this.inc);
   }
 
   render() {
     console.log('Hello Kitty');
-    return <button onClick={this.update}>{this.state.val}</button>
+    return <button onClick={this.update}>{this.state.val * this.state.m}</button>
   }
 }
 
