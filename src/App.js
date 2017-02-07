588e8f0ab7eb0f901ddd6cbefa67dda1cc90f468
Deepthi Kulkarni <deepthi.kulkarni@redbubble.com>
2017-02-08 10:09:15 +1100

    Chapter 9

--- src/App.js
+++ src/App.js
@@ -1,66 +1,28 @@
-// import React, { Component } from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-//
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <div className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <h2>Welcome to React</h2>
-//         </div>
-//         <p className="App-intro">
-//           To get started, edit <code>src/App.js</code> and save to reload. kjhsdjfh
-//         </p>
-//       </div>
-//     );
-//   }
-// }
-//
-// export default App;
-
 import React, { Component } from 'react';
 
 class App extends Component {
-
   constructor() {
     super();
-    this.state = {
-      txt: 'this is the state text',
-    }
+    this.state = { currentEvent: '---' }
+    this.update = this.update.bind(this);
   }
 
   update(e) {
-    console.log(e);
-    this.setState({txt: e.target.value});
+    this.setState({ currentEvent: e.type });
   }
 
   render() {
-    return (
-      <div>
-        <Button>I <Heart /> <b>React</b></Button>
-        <h1>{this.props.txt}</h1>
-        <b>{this.state.txt}</b>
-      </div>
-    )
+    return(<div>
+      <textarea
+        onKeyPress = {this.update}
+        onCut = {this.update}
+        onCopy = {this.update}
+        onPaste = {this.update}
+        cols="30"
+        rows="10" />
+      <h1>{this.state.currentEvent}</h1>
+    </div>);
   }
 }
 
-App.propTypes = {
-  txt: React.PropTypes.string,
-  age: React.PropTypes.number.isRequired,
-}
-
-App.defaultProps = {
-  txt: 'Default text'
-}
-
-const Button = (props) => <button>{props.children[0]}</button>
-
-class Heart extends Component {
-  render() {
-    return <span>&hearts;</span>;
-  }
-}
 export default App;
